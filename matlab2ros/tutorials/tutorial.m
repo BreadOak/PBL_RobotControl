6a57585eddcb4b7377363cb417303460b2a41c63
lgkimjy <lgkimjy@naver.com>
2021-11-29 13:13:55 +0900

    source updates

--- matlab2ros/tutorials/tutorial.m
+++ matlab2ros/tutorials/tutorial.m
@@ -1,16 +1,21 @@
 clear all
 clc
 
-testNode1 = ros2node("/test1")
+testNode1 = ros2node("/test1", 0)
 % sub = ros2subscriber(testNode1,"topic_2")
 % wait for 20 secs until message from Linux environment is received
 % msg = receive(sub, 20)
-pub = ros2publisher(testNode1,"/topic_1","std_msgs/String");
-pubMsg = ros2message("std_msgs/String");
-pubMsg.data = 'Hello';
+pub = ros2publisher(testNode1,"/topic_1","std_msgs/Float64");
+pubMsg = ros2message("std_msgs/Float64");
+pubMsg.data = 1;
+
+pub_1 = ros2publisher(testNode1,"/topic_2","std_msgs/Float64");
+pubMsg_2 = ros2message("std_msgs/Float64");
+pubMsg_2.data = 2;
 % publish 'Hello' 10 times to the /topic_1
-for x = 1:10
+for x = 1:10000
     send(pub,pubMsg)
+    send(pub_1,pubMsg_2)
     disp('msg sent')
-    pause(1)
+    pause(0.01)
 end
