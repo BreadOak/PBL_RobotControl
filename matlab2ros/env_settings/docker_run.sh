adf88ff3e50ddc302dfc454ab91053804691065b
lgkimjy <lgkimjy@naver.com>
2021-11-29 13:15:30 +0900

    update script files for build and run

---
+++ matlab2ros/env_settings/docker_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xhost +local:root
+
+docker run --rm -it -d \
+    --name gpu_ros2 \
+    --privileged \
+    --volume=/dev:/dev \
+    --net=host \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    lgkimjy/ubuntu:ros2_dashing
