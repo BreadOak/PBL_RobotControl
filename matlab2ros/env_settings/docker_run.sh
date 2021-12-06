03e3100bd938192fb1bf2f1daf96dcdec31bac03
lgkimjy <lgkimjy@naver.com>
2021-12-06 17:33:21 +0300

    add virtual share space

--- matlab2ros/env_settings/docker_run.sh
+++ matlab2ros/env_settings/docker_run.sh
@@ -9,4 +9,5 @@ docker run --rm -it -d \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
+    -v $HOME/docker_share:/home/junyoung/colcon_ws/src \
     lgkimjy/ubuntu:ros2_dashing
